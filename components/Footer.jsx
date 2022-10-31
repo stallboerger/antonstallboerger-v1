29f6dd482ef9ac69758025695d5fb70b14078a88
Anton <anton.stallboerger@hfg-gmuend.de>
2022-10-31 10:37:51 +0100

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 export const Footer = () => {
     return (
-        <div className='w-full h-40'>
+        <div className='w-full h-auto mb-20'>
             <div className="w-full h-[1px] bg-zinc-200 mb-6"></div>
-            <p className="credits text-sm text-zinc-500 mb-2">&copy; Anton Stallbörger 2022. This website is hosted and deployed via Vercel.</p>
+            <p className="credits text-sm text-zinc-500 mb-2">&copy; Anton Stallbörger 2022. This website is hosted and deployed via <Link href='https://vercel.com/' target="_blank" className="font-medium hover:underline">Vercel</Link>. Privacy-friendly Analytics by <Link href="https://vercel.com/analytics" target="_blank" className="font-medium hover:underline">Vercel Analytics</Link>.</p>
             <div className="flex gap-4 text-zinc-500 text-sm">
-                <Link href='/'>Legal Notice</Link>
-                <Link href='/'>Privacy Policy</Link>
+                <Link href='/' className="underline">Legal Notice</Link>
+                <Link href='/' className="underline">Privacy Policy</Link>
             </div>
         </div>
     )
