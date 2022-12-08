50199fd387d7c7538831c5bc2895c680a32e1f97
Anton <anton.stallboerger@hfg-gmuend.de>
2022-12-08 16:36:36 +0100

    time fix for GMT

--- components/GMTTime.jsx
+++ components/GMTTime.jsx
@@ -5,9 +5,12 @@ function GMTTime() {
   const hours = date.getUTCHours().toString().padStart(2, '0');
   const minutes = date.getUTCMinutes().toString().padStart(2, '0');
 
+  // add one hour to the time
+  const newHours = (parseInt(hours) + 1).toString().padStart(2, '0');
+
   return (
     <div>
-      <p className='text-sm text-zinc-500'> {hours}:{minutes}</p>
+      <p className='text-sm text-zinc-500'> {newHours}:{minutes}</p>
     </div>
   );
 }
