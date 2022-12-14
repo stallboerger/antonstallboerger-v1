9585c4ae28d57effefd045676c75cefe2da37b8c
Anton <anton.stallboerger@hfg-gmuend.de>
2022-12-14 10:30:10 +0100

    updated og image

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,9 @@ export default function Home() {
         <meta name="description" content="Digital Product Designer and Developer" />
         <meta
         property="og:image"
-        content="/assets/image.svg"
+        content="/assets/og_image.png"
         />
+        <meta name="twitter:image" content="/og_image.png"></meta>
         <meta name="theme-color" content="#FFFFFF"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
