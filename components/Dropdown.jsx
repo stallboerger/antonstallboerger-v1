bcaf1f4fccb5a46301cf6e1b0ea618f381a9be4e
Anton <anton.stallboerger@hfg-gmuend.de>
2023-01-01 12:24:10 +0100

    opacity fix

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -26,7 +26,7 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-4 w-56 origin-top-right divide-y divide-zinc-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-4 w-56 origin-top-right divide-y divide-zinc-100 rounded-md bg-white opacity-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
