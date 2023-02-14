691f123bc88cd4dabe1bd6256baba501bff120d4
Anton <anton.stallboerger@hfg-gmuend.de>
2023-02-14 16:03:04 +0100

    Adjust positioning of carousel navigation arrows.

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -23,10 +23,10 @@ const Carousel = () => {
   return (
     <div className="relative pb-6 z-0">
       <Image src={images[currentImage]} alt={`Image ${currentImage + 1}`} className="object-cover rounded-xl max-h-[340px]" width="593" height="340"/>
-      <button onClick={handlePreviousClick} className="absolute left-0 top-[42%] flex justify-between items-center ml-4 rounded-full bg-zinc-50/75 p-2" aria-label="Left">
+      <button onClick={handlePreviousClick} className="top-[50%] absolute -translate-y-[50%] left-0 ml-4 rounded-full bg-zinc-50/75 p-2" aria-label="Left">
         <NavArrowLeft className="h-6 w-6 stroke-2 text-zinc-800" />
       </button>
-      <button onClick={handleNextClick} className="absolute right-0 top-[42%] flex justify-between items-center mr-4 rounded-full bg-zinc-50/75 p-2" aria-label="Right">
+      <button onClick={handleNextClick} className="top-[50%] absolute -translate-y-[50%] right-0 mr-4 rounded-full bg-zinc-50/75 p-2" aria-label="Right">
         <NavArrowRight className="h-6 w-6 stroke-2 text-zinc-800" />
       </button>
     </div>
