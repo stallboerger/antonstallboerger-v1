1634ade92f81ca5f75400ce54ce43db99894f71a
Anton <anton.stallboerger@hfg-gmuend.de>
2023-03-02 09:00:02 +0100

    carousel update

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -23,11 +23,11 @@ const Carousel = () => {
   return (
     <div className="relative pb-6 z-0">
       <Image src={images[currentImage]} alt={`Image ${currentImage + 1}`} className="object-cover rounded-xl max-h-[340px]" width="593" height="340"/>
-      <button onClick={handlePreviousClick} className="top-[50%] absolute -translate-y-[50%] left-0 ml-4 rounded-full bg-zinc-50/75 p-2" aria-label="Left">
-        <NavArrowLeft className="h-6 w-6 stroke-2 text-zinc-800" />
+      <button onClick={handlePreviousClick} className="top-[50%] absolute -translate-y-[50%] left-0 ml-4 rounded-full bg-zinc-50/75 p-2 backdrop-blur-sm " aria-label="Left">
+        <NavArrowLeft className="h-6 w-6 stroke-2 text-zinc-800 -translate-x-[1px]" />
       </button>
-      <button onClick={handleNextClick} className="top-[50%] absolute -translate-y-[50%] right-0 mr-4 rounded-full bg-zinc-50/75 p-2" aria-label="Right">
-        <NavArrowRight className="h-6 w-6 stroke-2 text-zinc-800" />
+      <button onClick={handleNextClick} className="top-[50%] absolute -translate-y-[50%] right-0 mr-4 rounded-full bg-zinc-50/75 backdrop-blur-sm p-2" aria-label="Right">
+        <NavArrowRight className="h-6 w-6 stroke-2 text-zinc-800 translate-x-[1px]" />
       </button>
     </div>
   )
