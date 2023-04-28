9c348f776bb0c16eabc8dc40b96c7fdc5a027085
Anton <anton.stallboerger@hfg-gmuend.de>
2023-04-28 19:16:57 +0200

    .png

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Digital Product Designer and Developer" />
         <meta 
         property="og:image" 
-        content="https://www.antonstallboerger.com/og_image-png" 
+        content="https://www.antonstallboerger.com/og_image.png" 
         />
         <meta 
         name="twitter:image" 
