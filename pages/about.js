7c1accd6c32b3b5b79f7e93204f67cb2113bba70
Anton <anton.stallboerger@hfg-gmuend.de>
2023-04-28 18:55:49 +0200

    og image update

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Anton Stallbörger About Me</title>
+        <title>About | Anton Stallbörger</title>
         <meta name="description" content="Digital Product Designer and Developer" />
         <meta 
         property="og:image" 
--- pages/thoughts.js
+++ pages/thoughts.js
@@ -7,15 +7,15 @@ export default function Home() {
   return (
     <div>
       <Head>
-      <title>Thoughts Anton Stallbörger</title>
+        <title>Thoughts | Anton Stallbörger</title>
         <meta name="description" content="Digital Product Designer and Developer" />
         <meta 
         property="og:image" 
-        content="https://antonstallboerger.com/og_image.png" 
+        content="https://www.antonstallboerger.com/assets/og/og_thoughts.png" 
         />
         <meta 
         name="twitter:image" 
-        content="https://antonstallboerger.com/og_image.png" 
+        content="https://www.antonstallboerger.com/assets/og/og_thoughts.png" 
         />
         <meta name="theme-color" content="#FFFFFF"/>
         <link rel="icon" href="/favicon.ico" />
